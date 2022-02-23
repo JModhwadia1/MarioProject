e0a9742dcb44c15cd1276185f0f23cfa970bc133
JModhwadia1 <91890506+JModhwadia1@users.noreply.github.com>
2022-02-23 12:25:25 +0000

    Finished Tutorial 6
    Added ScreenManager class and

---
+++ MarioProject/MarioProject/Commons.h
@@ -0,0 +1,27 @@
+#pragma once
+struct Vector2D
+{
+	float x;
+	float y;
+	Vector2D()
+	{
+		x = 0.0f;
+		y = 0.0f;
+
+	}
+	Vector2D(float initial_x, float initital_y)
+	{
+		x = initial_x;
+		x = initital_y;
+	}
+
+};
+enum SCREENS
+{
+	SCREEN_INTRO,
+	SCREEN_MENU,
+	SCREEN_LEVEL1,
+	SCREEN_LEVEL2,
+	SCREEN_GAMEOVER,
+	SCREEN_HIGHSCORES
+};
